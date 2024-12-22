8978e79dfeba19449defc3a7efa377e801c5866a
Jashan <jashan6103@gmail.com>
2024-12-21 20:46:23 -0500

    Update axiosInstance.jsx

--- frontend/src/components/axiosInstance.jsx
+++ frontend/src/components/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://cfanbinv.onrender.com//api", // Set the base URL for all requests
+  baseURL: "https://cfanbinv.onrender.com/api", // Set the base URL for all requests
     // baseURL: "http://localhost:5000/api", // Set the base URL for
     withCredentials: true, // Send cookies when cross-origin
   headers: {
