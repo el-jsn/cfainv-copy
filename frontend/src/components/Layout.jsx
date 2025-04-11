8eccf99e88ca9e843aa5103bd0968a90b66849c2
Jashanpreet Singh <jashan6103@gmail.com>
2025-04-10 20:07:42 -0400

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -45,7 +45,7 @@ const Layout = React.memo(({ children }) => {
         dropdownLinks: [
           { to: "/update-sales-projection", label: "Update Sales Projections" },
           { to: "/thawing-cabinet/config", label: "Projection Rules" },
-          { to: "/truck-items", label: "Truck Order Calculator" },
+          { to: "/truck-items", label: "Truck Inventory" },
         ]
       },
       {
